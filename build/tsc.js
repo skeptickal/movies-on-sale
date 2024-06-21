0c47e25d6a6438e99a3cfc545976664df5beb271
Jackson Geer <jacksongeer@gmail.com>
2024-06-21 19:07:28 -0400

    added a fetch command to an api

--- build/tsc.js
+++ build/tsc.js
@@ -1,3 +1,13 @@
 var x = 'Hello World';
+var y = fetch('https://www.dnd5eapi.co/api/spells/acid-arrow')
+    .then(function (response) {
+    if (!response.ok) {
+        throw new Error('Network response was not ok');
+    }
+    return response.json();
+})
+    .then(function (data) { return console.log(data); })
+    .catch(function (error) { return console.error('Fetch error:', error); });
 console.log(x);
+console.log(y);
 //# sourceMappingURL=tsc.js.map
--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,14 @@
 const x: String = 'Hello World';
+
+const y = fetch('https://www.dnd5eapi.co/api/spells/acid-arrow')
+.then(response => {
+  if (!response.ok) {
+    throw new Error('Network response was not ok');
+  }
+  return response.json();
+})
+.then(data => console.log(data))
+.catch(error => console.error('Fetch error:', error));
+
 console.log(x);
+console.log(y)
